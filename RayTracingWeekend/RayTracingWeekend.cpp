1b376246ea6b8c0f407cd2351036a46ef3e16cf4
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-11-18 10:46:24 +1100

    Added starting project layout of ray tracing in a weekend blog

---
+++ RayTracingWeekend/RayTracingWeekend.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main()
+{
+    int image_width = 256;
+    int image_height = 256;
+
+    ofstream outputFile("outputFile.ppm");
+    outputFile << "P3" << endl << image_width << ' ' << image_height << endl << "255" << endl;
+    cout << "Started Image Processing" << endl;
+    for (int j = 0; j < image_height; j++) {
+        clog << "Image Progress " << ((float)j / (float)image_height * 100) << endl;
+        
+        for (int i = 0; i < image_width; i++) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+            
+            outputFile << ir << ' ' << ig << ' ' << ib;
+            outputFile << endl;
+        }
+    }
+    clog << "Image Progress Complete" << endl;
+    outputFile.close();
+}
