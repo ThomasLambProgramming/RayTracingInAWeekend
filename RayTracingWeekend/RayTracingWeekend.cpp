027d93d3966617cf4e4f7c8e6a3b879e8d74252b
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-11-30 01:32:18 +1100

    Added ray sphere intersection

--- RayTracingWeekend/RayTracingWeekend.cpp
+++ RayTracingWeekend/RayTracingWeekend.cpp
@@ -6,8 +6,33 @@
 
 using namespace std;
 
+double RaySphereCollisionCheck(const Point3& Center, double Radius, const Ray& ray)
+{
+    //I do not fully understand the math behind this but the following is to find how far along a ray (if at all) is the sphere intersecting with it.
+    Vector3 oc = Center - ray.Origin();
+    double a = ray.Direction().SquaredLength();
+    double h = DotProduct(ray.Direction(), oc);
+    double c = oc.SquaredLength() - (Radius * Radius);
+    
+    double discriminant = (h * h) -  (a * c);
+    if (discriminant < 0)
+        return -1.0;
+
+    return (h - std::sqrt(discriminant)) / a; 
+}
+
 Color RayColor(const Ray& r)
 {
+    double t = RaySphereCollisionCheck(Point3(0,0,-1), 0.5, r);
+    if (t > 0.0)
+    {
+        //Position of collision with ray and sphere - sphere position to get normal of sphere.
+        Vector3 normal = UnitVector(r.PositionAt(t) - Vector3(0,0,-1));
+
+        //Return normal put back into range of [0,1]
+        return 0.5 * Color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
+    }
+    
     Vector3 direction = UnitVector(r.Direction());
     double a = 0.5 * (direction.y() + 1.0);
     return (1.0 - a) * Color(1.0,1.0,1.0) + a * Color(0.5,0.7,1.0);
@@ -42,11 +67,13 @@ int main()
     Vector3 pixel00Location = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
 
     ofstream outputFile("outputFile.ppm");
-    outputFile << "P3" << endl << imageWidth << ' ' << imageHeight << endl << "255" << endl;
-    cout << "Started Image Processing" << endl;
-    for (int j = 0; j < imageHeight; j++) {
-        clog << "Image Progress " << ((float)j / (float)imageHeight * 100) << endl;
-        for (int i = 0; i < imageWidth; i++) {
+    outputFile << "P3" << '\n' << imageWidth << ' ' << imageHeight << "\n" << "255" << "\n";
+    cout << "Started Image Processing" << "\n";
+    for (int j = 0; j < imageHeight; j++)
+    {
+        clog << "Image Progress " << (int)((float)j / imageHeight * 100) << "\n";
+        for (int i = 0; i < imageWidth; i++)
+        {
             Point3 pixelCenter = pixel00Location + (i * pixelDeltaU) + (j * pixelDeltaV);
             //direction from camera to the viewport pixel center
             Vector3 rayDirection = pixelCenter - cameraCenter;
@@ -58,6 +85,7 @@ int main()
             WriteColor(outputFile ,pixelColor);
         }
     }
-    clog << "Image Progress Complete" << endl;
+    
+    clog << "Image Progress Complete" << "\n";
     outputFile.close();
 }
